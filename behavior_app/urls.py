72874e184d75445f880bddfd3d87b2ab92f0cda7
Morris <jmorris@tchl-25409.local>
2023-07-20 13:53:56 -0500

    Included adding students and adding notes

--- behavior_app/urls.py
+++ behavior_app/urls.py
@@ -1,25 +1,25 @@
-
-# behavior_app/urls.py
 from django.urls import path
 from .views import (
     student_list,
     student_detail,
-    note_create,
-    note_update,
-    note_delete,
-    top_behaviors,
+    add_students_manually,
+    delete_student,
+    add_students_with_csv,
+    notes_list,
+    NoteAddView,  # Include the new view
 )
 
 app_name = 'behavior_app'
 
 urlpatterns = [
+    path('', student_list, name='default-page'),
     path('students/', student_list, name='student-list'),
     path('students/<int:pk>/', student_detail, name='student-detail'),
-    path('notes/add/', note_create, name='note-add'),
-    path('notes/<int:pk>/edit/', note_update, name='note-edit'),
-    path('notes/<int:pk>/delete/', note_delete, name='note-delete'),
-    path('top-behaviors/', top_behaviors, name='top-behaviors'),
+    path('students/add-manually/', add_students_manually, name='add-students-manually'),
+    path('students/<int:pk>/delete/', delete_student, name='delete-student'),
+    path('students/add-with-csv/', add_students_with_csv, name='add-students-with-csv'),
+    path('students/<int:pk>/notes/', notes_list, name='notes-list'),
 
-    # Empty path acts as the catch-all or default URL
-    path('', student_list, name='default-page'),
+    # Use NoteAddView for "note-add"
+    path('notes/add/<int:pk>/', NoteAddView.as_view(), name='note-add'),
 ]
--- behavior_app/views.py
+++ behavior_app/views.py
@@ -1,7 +1,33 @@
 # behavior_app/views.py
+import csv
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student, Note, Behavior
+from .forms import NoteForm, BehaviorForm
+
+# behavior_app/views.py
+from django.shortcuts import render
+
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 from .forms import NoteForm
+from .models import Student
+
+
+class NoteAddView(FormView):
+    template_name = 'note_form.html'
+    form_class = NoteForm
+    success_url = reverse_lazy('behavior_app:student-list')
+
+    def form_valid(self, form):
+        student_id = self.kwargs['pk']
+        student = get_object_or_404(Student, pk=student_id)
+        form.instance.student = student
+        form.save()
+        return super().form_valid(form)
+
+
+def default_page(request):
+    return render(request, 'default_page.html')
 
 
 def student_list(request):
@@ -11,41 +37,41 @@ def student_list(request):
 
 def student_detail(request, pk):
     student = get_object_or_404(Student, pk=pk)
-    notes = Note.objects.filter(student=student)
+    notes = Note.objects.filter(student=student).order_by('-date')
     return render(request, 'student_detail.html', {'student': student, 'notes': notes})
 
 
-def note_create(request):
+def add_students_manually(request):
     if request.method == 'POST':
-        form = NoteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('behavior_app:student-list')
-    else:
-        form = NoteForm()
-    return render(request, 'note_form.html', {'form': form})
-
-
-def note_update(request, pk):
-    note = get_object_or_404(Note, pk=pk)
-    if request.method == 'POST':
-        form = NoteForm(request.POST, instance=note)
-        if form.is_valid():
-            form.save()
-            return redirect('behavior_app:student-list')
-    else:
-        form = NoteForm(instance=note)
-    return render(request, 'note_form.html', {'form': form})
-
-
-def note_delete(request, pk):
-    note = get_object_or_404(Note, pk=pk)
-    if request.method == 'POST':
-        note.delete()
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        student = Student.objects.create(first_name=first_name, last_name=last_name)
         return redirect('behavior_app:student-list')
-    return render(request, 'note_confirm_delete.html', {'note': note})
+    return render(request, 'add_students_manually.html')
 
 
-def top_behaviors(request):
-    behaviors = Behavior.objects.annotate(num_occurrences=Count('note')).order_by('-num_occurrences')[:5]
-    return render(request, 'top_behaviors.html', {'behaviors': behaviors})
+def delete_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('behavior_app:student-list')
+    return render(request, 'delete_student.html', {'student': student})
+
+
+def add_students_with_csv(request):
+    if request.method == 'POST' and request.FILES.get('csv_file'):
+        csv_file = request.FILES['csv_file']
+        decoded_file = csv_file.read().decode('utf-8')
+        csv_reader = csv.reader(decoded_file.splitlines(), delimiter=',')
+        next(csv_reader)  # Skip header row
+        for row in csv_reader:
+            first_name, last_name = row
+            Student.objects.create(first_name=first_name, last_name=last_name)
+        return redirect('behavior_app:student-list')
+    return render(request, 'add_students_with_csv.html')
+
+
+def notes_list(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    notes = Note.objects.filter(student=student).order_by('-date')
+    return render(request, 'notes_list.html', {'student': student, 'notes': notes})
